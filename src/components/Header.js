5e2d1e92597000ca3f96270dab345e022518488e
breadfan18 <breadfan18@gmail.com>
2022-03-28 19:00:52 -0400

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => (
     <header>
         {
             props.user ? 
-                <Link to='/' onClick={logout} style={{color: 'white'}}>Log Out</Link>
+                <Link to='/' onClick={logout}>Log Out</Link>
             :
             null
         }    
