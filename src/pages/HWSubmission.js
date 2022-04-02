4766614741f828e4fc1e19e775981ac14591a7a0
Swaroop Uprety <breadfan18@gmail.com>
2022-04-02 16:28:23 -0400

    basic hw submission form

--- src/pages/HWSubmission.js
+++ src/pages/HWSubmission.js
@@ -1,7 +1,33 @@
 // Components
 const HWSubmission = props => {
     return (
-        <h1>Submit a Homework</h1>
+        <div>
+            <h1>Submit a Homework</h1>
+            <form>
+                <label>
+                    First Name:
+                    <input type="text" name="firstName" />
+                </label>
+                <label>
+                    Last Name:
+                    <input type="text" name="lastName" />
+                </label>
+                <label>
+                    Homework:
+                    <select name="homework">
+                        <option value="homework1">Homework 1</option>
+                        <option value="homework2">Homework 2</option>
+                        <option value="homework3">Homework 3</option>
+                        <option value="homework4">Homework 4</option>
+                    </select>
+                </label>
+                <label>
+                    Repo Link:
+                    <input type="text" name="repoLink" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
 
     )
 }
